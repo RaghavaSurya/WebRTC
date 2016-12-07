771d3ae1b4e7bb98901478d06d4c6082ee86db52
Surya <asfd.afsdfas@gov.in>
2016-12-07 15:53:10 +0530

    Initial Commit

---
+++ index.js
@@ -0,0 +1,38 @@
+navigator.webkitGetUserMedia({ video: true, audio: true }, function (stream) {
+
+    var Peer = require('simple-peer');
+    var peer = new Peer({
+        initiator: location.hash === '#init',
+        trickle: false,
+        stream: stream
+    });
+
+    peer.on('signal', function (data) {
+        document.getElementById('yourId').value = JSON.stringify(data);
+        console.log(data);
+    })
+
+    document.getElementById('connect').addEventListener('click', function () {
+        var othersId = JSON.parse(document.getElementById('otherId').value)
+        peer.signal(othersId);
+    })
+
+    document.getElementById('send').addEventListener('click', function () {
+        var yourMessage = document.getElementById('yourMessage').value
+        peer.send(yourMessage);
+    })
+
+    peer.on('data', function (data) {
+        document.getElementById('messages').textContent += data + '\n'
+    })
+
+    peer.on('stream',function(stream){
+        var video=document.createElement('video');
+        document.body.appendChild(video)
+
+        video.src=window.URL.createObjectURL(stream)
+        video.play();
+    })
+}, function (err) {
+    console.error(err);
+})
